ec6fe221ee8df237cc69292be2bdadf26ab22c35
Guilherme Medeiros <contato@frenetic.com.br>
2022-08-01 12:35:11 -0300

    Exercício dois - Fahrenheit para Celsius

---
+++ 02/exercicio.test.ts
@@ -0,0 +1,15 @@
+import { fahrenheitParaCelsius } from "./exercicio";
+
+describe("Exercício 02 - Fahrenheit para Celsius", () => {
+    test("0 °F para °C", () => {
+        fail("implemente seu teste");
+    });
+
+    test("32 °F para °C", () => {
+        fail("implemente seu teste");
+    });
+
+    test("100 °F para °C", () => {
+        fail("implemente seu teste");
+    });
+});
---
+++ 02/exercicio.ts
@@ -0,0 +1,3 @@
+export function fahrenheitParaCelsius(grau: number): number {
+    return grau;
+}
