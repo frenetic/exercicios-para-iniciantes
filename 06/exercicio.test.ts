16ab2d5789078cabf9584ab7f9253be47eacbf68
Guilherme Medeiros <contato@frenetic.com.br>
2022-08-01 19:51:17 -0300

    Exercício 06 - Validação de triângulo retângulo

---
+++ 06/exercicio.test.ts
@@ -0,0 +1,20 @@
+import { validaTrianguloRetangulo } from "./exercicio";
+
+describe("Exercício 06 - Validação de triângulo retângulo", () => {
+    test("Lados 0, 0, 0", () => {
+        fail("implemente seu teste");
+    });
+
+    test("Lados 1, 1, 1", () => {
+        fail("implemente seu teste");
+    });
+
+    test("Lados 3, 4, 5", () => {
+        fail("implemente seu teste");
+    });
+
+    test("Lados 5, 4, 3", () => {
+        fail("implemente seu teste");
+    });
+});
+
